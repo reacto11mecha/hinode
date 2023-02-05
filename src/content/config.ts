1ed859f1baac65b39aa386c29664ee6c19fb9907
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-02-06 05:10:53 +0700

    feat: pindah ke content collection

---
+++ src/content/config.ts
@@ -0,0 +1,13 @@
+import { z, defineCollection } from "astro:content";
+
+const materiCollection = defineCollection({
+  schema: z.object({
+    layout: z.string().startsWith("../../../layouts/Presentation"),
+    title: z.string(),
+    description: z.string(),
+  }),
+});
+
+export const collections = {
+  materi: materiCollection,
+};
