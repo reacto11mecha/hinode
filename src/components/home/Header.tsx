e5ced16bfddf628366ac6669ed80126a25c5020b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-10-12 14:33:55 +0700

    feat: menambahkan gambar

--- src/components/home/Header.tsx
+++ src/components/home/Header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <header id="hero" className="h-screen bg-cover bg-header">
+    <header className="h-screen bg-cover bg-header">
       <div className="backdrop-brightness-50 w-full h-full flex justify-center items-center md:justify-start md:items-end md:pb-20 md:pl-10">
         <h1 className="flex flex-col gap-5 text-center font-bold text-white text-7xl md:text-left md:gap-7">
           <span className="font-zenka [text-shadow:_10px_0_rgba(0,0,0,.4)]">
