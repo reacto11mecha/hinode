f1210d6b73d99eb114ea97bbab90de3c06025b4b
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-11-11 10:30:43 +0700

    perf: improve performa load gambar

--- src/components/keanggotaan/Member.tsx
+++ src/components/keanggotaan/Member.tsx
@@ -1,38 +1,62 @@
 import type { IMember } from "@/assets/pengurus";
 
-export const Member = ({ katakanaMax, memberData, children }: {
-  katakanaMax: number; memberData: IMember; children: React.ReactNode
-}) => (
-  <button
-    className="bg-slate-100 dark:bg-stone-800 text-black w-[18em] h-[23em] flex flex-col items-center justify-end"
-    onClick={() => {
-      const toggle = (window as unknown as { toggleModal: () => void }).toggleModal;
+import { useRef } from "react";
 
-      const imageEl = document.querySelector(".modal img")! as HTMLImageElement;
-      imageEl.src = memberData.imageModal;
+export const Member = ({
+  katakanaMax,
+  memberData,
+  children,
+}: {
+  katakanaMax: number;
+  memberData: IMember;
+  children: React.ReactNode;
+}) => {
+  const btnRef = useRef(null!);
 
-      const normalName = document.querySelector(".modal #normal-name")!;
-      normalName.textContent = memberData.name;
+  return (
+    <button
+      ref={btnRef}
+      className="bg-slate-100 dark:bg-stone-800 text-black w-[18em] h-[23em] flex flex-col items-center justify-end"
+      onClick={(e) => {
+        const toggle = (window as unknown as { toggleModal: () => void })
+          .toggleModal;
 
-      const nihonName = document.querySelector(".modal #nihon-name")!;
-      nihonName.textContent = memberData.katakana;
+        const imageSource = btnRef.current.querySelector(
+          ".hidden",
+        )! as HTMLImageElement;
 
-      const message = document.querySelector(".modal #message")!;
-      message.textContent = memberData.message;
+        const imageEl = document.querySelector(
+          ".modal img",
+        )! as HTMLImageElement;
+        imageEl.src = imageSource.src;
+        imageEl.alt = imageSource.alt;
 
-      const position = document.querySelector(".modal #position")!;
-      position.textContent = memberData.position;
+        const normalName = document.querySelector(".modal #normal-name")!;
+        normalName.textContent = memberData.name;
 
-      toggle();
-    }}
-  >
-    {children}
-    <div className="flex flex-row w-full">
-      {Array.from({ length: katakanaMax }).map((_, idx) => (
-        <div key={idx} className="h-10 border-[0.1px] w-full flex dark:text-white items-center justify-center font-zenka font-semibold text-xl">
-          {memberData.katakana.split("")[idx] ?? ""}
-        </div>
-      ))}
-    </div>
-  </button>
-);
+        const nihonName = document.querySelector(".modal #nihon-name")!;
+        nihonName.textContent = memberData.katakana;
+
+        const message = document.querySelector(".modal #message")!;
+        message.textContent = memberData.message;
+
+        const position = document.querySelector(".modal #position")!;
+        position.textContent = memberData.position;
+
+        toggle();
+      }}
+    >
+      {children}
+      <div className="flex flex-row w-full">
+        {Array.from({ length: katakanaMax }).map((_, idx) => (
+          <div
+            key={idx}
+            className="h-10 border-[0.1px] w-full flex dark:text-white items-center justify-center font-zenka font-semibold text-xl"
+          >
+            {memberData.katakana.split("")[idx] ?? ""}
+          </div>
+        ))}
+      </div>
+    </button>
+  );
+};
